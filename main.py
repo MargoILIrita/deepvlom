04728dbe2687a9ec68a780e1341c584839379568
Margarita <rita96pol@gmail.com>
2018-05-26 16:47:53 +0400

    commments

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import subprocess
 import collections
 import sys
 
+#получение текста из docx
 def getText(filename):
     doc = docx.Document('docx/' + filename + '.docx')
     fullText = []
@@ -11,15 +12,19 @@ def getText(filename):
     return '\n'.join(fullText)
 
 
+#получение файла txt
 def converttotxt(filename):
     f = open('txt/' + filename + '.txt', 'w', encoding='utf-8')
     f.write(getText(filename))
     f.close()
 
+
+#запуск mystem плучение лемм
 def mystem(filename):
     converttotxt(filename)
     args = 'mystem/mystem.exe -n -w -l -d  txt/' + filename + '.txt results/mystem/' + filename + '.txt'
     subprocess.call(args)
+
 
 def compute_tf(text):
     tf_text = collections.Counter(text)
@@ -28,23 +33,14 @@ def compute_tf(text):
     return tf_text
 
 
-def tf(filename):
-    f = open('results/mystem/' + filename + '.txt', 'r', encoding='utf-8')
-    text = []
-    for line in f:
-        if '??' not in line:
-            text.append(line.replace('\n', ''))
-    return compute_tf(text)
-
-
 if __name__ == '__main__':
     args = 'mystem/mystem.exe -n -w -l -d  txt/first.txt'
     with subprocess.call(args) as pse:
         text = []
         for line in pse:
-            if '??' not in line:
+            if line.find("??") != -1:
                 text.append(line.replace('\n', ''))
-        print( compute_tf(text))
+        print(compute_tf(text))
 
 
 
